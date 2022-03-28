8dea8073c609ceb05b6392b2b784646028982f24
Denis Gaščić <denisgascic9@gmail.com>
2022-03-29 00:37:56 +0200

    prijava gotova

---
+++ vueVolEvid/src/views/Prijava.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Prijavite se</h1>
+  <p><input type="text"  placeholder="e-mail" v-model="email" /></p>
+  <p><input type="password"  placeholder="Lozinka" v-model="password" /></p>
+<p><button @click="prijava">Prijava</Putton></p>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import {useRouter} from 'vue-router'
+import { error } from "console"
+const email=ref("")
+const password= ref("")
+const router = useRouter()
+const prijava = () => {
+    signInWithEmailAndPassword(auth(), email.value, password.value)
+    .then((data))=> {
+        console.log("Uspješna prijava");
+        router.push('/probni');
+    .catch ((error)) => console.log(error.code)
+    }
+}
+
+</script>
+
+<style>
+
+</style>
